87ff536de09685bc5d9bb2a5db6d308976ba33d4
Mikhail Safir <mikhail.safir@walmart.com>
2025-02-05 14:12:11 +0100

    Initial Commit

---
+++ Shapr3DConverter/Shapr3DConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shapr3DConverterApp.swift
+//  Shapr3DConverter
+//
+//  Created by Mikhail Safir - Vendor on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Shapr3DConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shapr3DConverterUITests/Shapr3DConverterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Shapr3DConverterUITestsLaunchTests.swift
+//  Shapr3DConverterUITests
+//
+//  Created by Mikhail Safir - Vendor on 05/02/2025.
+//
+
+import XCTest
+
+final class Shapr3DConverterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
